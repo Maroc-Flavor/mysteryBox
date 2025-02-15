56f010b2e25003888bcd52d6354b75d4284811a5
Maroc-Flavor <147657203+Maroc-Flavor@users.noreply.github.com>
2025-02-15 10:23:58 +0000

    Deploying to gh-pages from @ Maroc-Flavor/mysteryBox@7ad278239f35afcb68e24ddba39ed4061cd35b68 🚀

---
+++ _next/static/chunks/app/shop/page-e07a44adf152a88b.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[21],{2731:function(e,n,s){Promise.resolve().then(s.t.bind(s,413,23)),Promise.resolve().then(s.t.bind(s,8326,23)),Promise.resolve().then(s.bind(s,6128))}},function(e){e.O(0,[258,128,971,472,744],function(){return e(e.s=2731)}),_N_E=e.O()}]);
